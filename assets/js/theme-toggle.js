0b7b7a71522f3160934ee7447a59358a4bb34e72
Edna-Moraa <ednermoraa65@gmail.com>
2025-05-13 16:48:33 +0300

    updated portfolio

---
+++ assets/js/theme-toggle.js
@@ -0,0 +1,8 @@
+const toggle = document.getElementById("themeToggle");
+const root = document.documentElement;
+
+toggle.addEventListener("click", () => {
+  const currentTheme = root.getAttribute("data-theme");
+  const newTheme = currentTheme === "dark" ? "light" : "dark";
+  root.setAttribute("data-theme", newTheme);
+});
